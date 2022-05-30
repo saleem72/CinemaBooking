fbe4cc8ef92a4fe6893318e296f98bce048847d2
Nabil_saadi <nabil.saadi@codersdc.com>
2022-05-30 15:33:51 +0300

    Initial Commit

---
+++ CinemaBooking/CinemaBookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CinemaBookingApp.swift
+//  CinemaBooking
+//
+//  Created by Yousef on 5/30/22.
+//
+
+import SwiftUI
+
+@main
+struct CinemaBookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
